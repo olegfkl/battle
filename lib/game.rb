33c6d2d6255ee7ad6a85958215593f23ab67200d
Tom Moir <trmoir9@gmail.com>
2017-11-10 11:05:41 +0000

    working through switch method in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,31 @@
 class Game
 
-attr_reader :player1, :player2
+attr_reader :player1, :player2, :p2_turn
 
   def initialize(name1, name2)
     @player1 = name1
     @player2 = name2
+    @p1_turn = true
+    @p2_turn = false
   end
 
   def attack(player)
    player.receive_damage
   end
+
+  def switch
+    if @p1_turn
+      attack(player2)
+      @p1_turn = false
+      @p2_turn = true
+    else
+      attack(player1)
+      @p1_turn = true
+      @p2_turn = false
+    end
+  end
+
+
 
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,17 @@ describe Game do
       expect(game.attack(obj1)).to eq true
     end
   end
+
+  describe '#switch' do
+    context 'when it is player1s turn' do
+      it 'attacks player2' do
+        expect(obj2).to receive(:receive_damage)
+        game.switch
+      end
+
+      it 'switches to player2s turn' do
+        expect { game.switch }.to change { game.p2_turn }.to be(true)
+      end
+    end
+  end
 end
